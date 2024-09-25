88ce9a8a7058767e9fdc0187b7e59d1011112902
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-09-25 16:09:32 -0500

    end of class commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const button = document.querySelector('#btn')
+const pokeInput = document.querySelector('#searchbar')
+const imageContainer = document.querySelector('#imgContainer')
+const finalName = document.querySelector('#name')
+const finalEvolutions = document.querySelector('#evolutions')
+const finalTypes = document.querySelector('#types')
+const finalWeight = document.querySelector('#weight')
+const finalHeight = document.querySelector('#height')
+const finalFT = document.querySelector('#flavorText')
+
+button.addEventListener('click', async () => {
+    let pokemon = pokeInput.value.toLowerCase()
+
+    let response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    )
+    console.log(response)
+
+    let pokeImg = response.data.sprites.front_default
+    imageContainer.setAttribute('src', `${pokeImg}`)
+
+    let weight = response.data.weight
+    weight = weight /10
+    finalWeight.innerText = `Weight: ${weight}kg`
+
+    let height = response.data.height
+    height = height /10
+    finalHeight.innerText = `Height: ${height}m`
+
+    let typesArray = response.data.types
+    finalTypes.textContent = 'Type(s): '
+    for (typeObject of typesArray) {
+        finalTypes.append(`${typeObject.type.name} `)
+    }
+
+    let speciesResponse = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon-species/${pokemon}/`
+    )
+    console.log(speciesResponse)
+
+    let name = speciesResponse.data.name
+    finalName.innerText = `Species: ${name}`
+
+    //below does not work if the pokemon is the first evolution, it then breaks the rest of this
+    // let evolve = speciesResponse.data.evolves_from_species.name
+    // finalEvolutions.innerText = `Evolves from: ${evolve}`
+
+    let flavorTextArray = speciesResponse.data.flavor_text_entries
+    let flavorText = ""
+    for (i=0; i<flavorTextArray.length; i++) {
+        if (i === 1) {
+            flavorText = flavorTextArray[i].flavor_text
+        }
+    }
+    finalFT.innerText = flavorText
+    console.log(flavorTextArray)
+})
